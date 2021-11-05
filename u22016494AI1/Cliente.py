24d7e4a3096a9d76954b662df47986614c4edefa
adriDiazz <adriandiazmanzanares9@gmail.com>
2021-11-05 21:42:10 +0100

    Comentarios e inputs añadidos

--- u22016494AI1/Cliente.py
+++ u22016494AI1/Cliente.py
@@ -6,29 +6,30 @@ import os
 
 class Cliente():
     
-    def __init__(self, host=socket.gethostname(), port=59988):
+    #Inicizlizamos pasandole mediante inout del usuario el host(ip) y el puerto deseado
+    def __init__(self, host = input("Introduzca la IP:"), port = int(input("Introduzca el puerto deseado:"))):
 
-        nickname = input("Nickname:")
+        nickname = input("Nickname:")#Pedimos al usuario el nickname para que pueda identificarse antes de entrar al chat
         
-        self.sock = socket.socket()
-        self.sock.connect((str(host), int(port)))
-        self.sock.send(pickle.dumps(nickname))
-        hilo_recv_mensaje = threading.Thread(target=self.recibir)
+        self.sock = socket.socket()#Creamos el socket
+        self.sock.connect((str(host), int(port)))#Nos conectamos al host remoto mediante los datos recividos en los inputs
+        self.sock.send(pickle.dumps(nickname))#Enviamos el nickname para posteriormente poder mostrar los usuarios conectados
+        hilo_recv_mensaje = threading.Thread(target=self.recibir)#Creamos un hilo que tratara de recibir mensajes continuamente para ver si nos llega alguno 
         hilo_recv_mensaje.daemon = True
         hilo_recv_mensaje.start()
         print('Hilo con PID',os.getpid())
         print('Hilos activos', threading.active_count())
 
         while True:
-            msg = input('\nEscriba texto ? ** Enviar = ENTER ** Abandonar Chat = Q \n')
+            msg = input('\nEscriba texto ? ** Enviar = ENTER ** Abandonar Chat = Q \n')#Pedimos el mensaje al usuario
             if msg != 'Q' :
-                self.enviar(nickname + ": " + msg)
+                self.enviar(nickname + ": " + msg)#Si es distinto de Q el mensaje se enviara
             else:
                 print(" **** TALOGOOO  ****")
                 self.sock.close()
                 sys.exit()
     
-    def recibir(self):
+    def recibir(self):#Funcion para recibir mensajes que mandaremos ejecutar al hilo 
         while True:
             try:
                 data = self.sock.recv(32)
@@ -37,7 +38,7 @@ class Cliente():
             except:
                 pass
 
-    def enviar(self, msg):
+    def enviar(self, msg):#Funcion para enviar el mensaje mediante la libreria pickle para que la informacion viaje serializada
         self.sock.send(pickle.dumps(msg))
 
 c=Cliente()
--- u22016494AI1/Servidor.py
+++ u22016494AI1/Servidor.py
@@ -4,20 +4,21 @@ import sys
 import pickle
 import os
 
-
+#59986
 
 
 
 class Servidor():
 
-    def __init__(self, host=socket.gethostname(), port=59988):
+    #Inicializamos obteniendo nuestra ip(getHostName) y pidiendo el puerto al usuario
+    def __init__(self, host=socket.gethostname(), port = int(input("Introduzca el puerto deseado:"))):
         self.clientes = []
-        self.nicknames = []
+        self.nicknames = []#Creamos un array para guaradr los usuarios conectados
         self.sock = socket.socket()
-        self.sock.bind((str(host), int(port)))
-        self.sock.listen(20)
+        self.sock.bind((str(host), int(port)))#Asignamos la direccion y el puerto a la que el cliente se podra conectar 
+        self.sock.listen(20)#Especifica el número de conexiones no aceptadas que permitirá el sistema antes de rechazar nuevas conexiones. 
         self.sock.setblocking(False)
-        aceptar = threading.Thread(target=self.aceptarC)
+        aceptar = threading.Thread(target=self.aceptarC)#Ponemos dos hilos a aceptar y procesar mensajes continuamente para cuando alguno llegue
         procesar = threading.Thread(target=self.procesarC)
 
         aceptar.daemon = True
@@ -28,6 +29,7 @@ class Servidor():
 
         while True:
             msg = input('SALIR = Q\n')
+           
             if msg == 'Q':
                 print("**** TALOGOOO *****")
                 self.sock.close()
@@ -35,11 +37,13 @@ class Servidor():
             else:
                 pass
 
-    def broadcast(self, msg, cliente):
+    def broadcast(self, msg, cliente):#Funcion que encia a todos los clientes conectados el mensaje menos al que lo envia
         for c in self.clientes:
             try:
                 if c != cliente:
                     c.send(msg)
+                   
+
             except:
                 self.clientes.remove(c)
             
@@ -49,12 +53,12 @@ class Servidor():
                 conn, addr = self.sock.accept()
                 print(f"\nConexion aceptada via {conn}\n")
                 conn.setblocking(False)
-                data = conn.recv(32)
-                self.nicknames.append(data)
+                data = conn.recv(32)#Recogemos el nickname
+                self.nicknames.append(data)#Metemos el nickname en el array
                 print("Usuarios Conectados: ")
                 for n in self.nicknames:
-                    print(pickle.loads(n))
-                self.clientes.append(conn)
+                    print(pickle.loads(n))#Recorremos el array nicknames printeando el nickname deserializado
+                self.clientes.append(conn)#Anadimos el cliente a la lista de clientes 
                 
             except:
                 pass
@@ -62,13 +66,19 @@ class Servidor():
 
     def procesarC(self):
         print("Procesamiento de mensajes iniciado")
+        print(socket.gethostbyname(socket.gethostname()))#Printeamos la ip
         while True:
             if len(self.clientes) > 0:
                 for c in self.clientes:
                     try:
                         data = c.recv(32)
+                        
                         if data:
+                            with open('u22016494.txt','a') as writer:#Guardamos los mensajes en el .txt mediante el with para que se cierre automaticamente
+                                 writer.write(pickle.loads(data) + "\n")#Debemos de guardar el mensaje deserializado 
                             self.broadcast(data,c)
+                            
+                            
 
                     except:
                         pass
